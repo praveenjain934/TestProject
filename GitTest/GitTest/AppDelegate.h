8e205e4b1fd86b44618a26bdb630688d9ee08126
Praveen Jain <praveen@Admins-MacBook-Pro-3.local>
2017-02-01 16:11:01 +0530

    First commit

---
+++ GitTest/GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by Praveen Jain  on 2/1/17.
+//  Copyright © 2017 Praveen Jain . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTest/GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by Praveen Jain  on 2/1/17.
+//  Copyright © 2017 Praveen Jain . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
